36598a8943dd36a4ade75d24f1d21847bcbbdf17
Jaakko <jaakko.lipponen01@gmail.com>
2023-11-26 10:24:27 +0200

    formatting

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,59 +1,59 @@
-document.getElementById('input-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const recipe = document.getElementById("recipe-input").value;
-    const recipeDiv = document.getElementById("recipe-div");
+document.getElementById("input-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const recipe = document.getElementById("recipe-input").value;
+  const recipeDiv = document.getElementById("recipe-div");
 
-    // get recipe data
-    fetch(`/recipe/${recipe}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('fetch failed');
-            } 
-            return response.json();
-        })
-        .then(recipedata => {
-            console.log(recipedata);
-            recipeDiv.innerHTML = ""; //empty div
+  // get recipe data
+  fetch(`/recipe/${recipe}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("fetch failed");
+      }
+      return response.json();
+    })
+    .then((recipedata) => {
+      console.log(recipedata);
+      recipeDiv.innerHTML = ""; //empty div
 
-            // Display recipe name
-            const nameH3 = document.createElement('h3');
-            nameH3.textContent = "Name";
-            recipeDiv.appendChild(nameH3);
+      // Display recipe name
+      const nameH3 = document.createElement("h3");
+      nameH3.textContent = "Name";
+      recipeDiv.appendChild(nameH3);
 
-            const name = document.createElement('ul');
-            const nameLi = document.createElement('li');
-            nameLi.textContent = recipedata.name;
-            name.appendChild(nameLi);
-            recipeDiv.appendChild(name);
+      const name = document.createElement("ul");
+      const nameLi = document.createElement("li");
+      nameLi.textContent = recipedata.name;
+      name.appendChild(nameLi);
+      recipeDiv.appendChild(name);
 
-            // Display instructions
-            const instructionsH3 = document.createElement('h3');
-            instructionsH3.textContent = "Instructions";
-            recipeDiv.appendChild(instructionsH3);
+      // Display instructions
+      const instructionsH3 = document.createElement("h3");
+      instructionsH3.textContent = "Instructions";
+      recipeDiv.appendChild(instructionsH3);
 
-            const instructionsUl = document.createElement('ul');
-            recipeDiv.appendChild(instructionsUl);
+      const instructionsUl = document.createElement("ul");
+      recipeDiv.appendChild(instructionsUl);
 
-            recipedata.instructions.forEach(element => {
-                const li = document.createElement('li');
-                li.textContent = element;
-                instructionsUl.appendChild(li);
-            });
+      recipedata.instructions.forEach((element) => {
+        const li = document.createElement("li");
+        li.textContent = element;
+        instructionsUl.appendChild(li);
+      });
 
-            // Display ingredients
-            const ingredientsH3 = document.createElement('h3');
-            ingredientsH3.textContent = "Ingredients";
-            recipeDiv.appendChild(ingredientsH3);
+      // Display ingredients
+      const ingredientsH3 = document.createElement("h3");
+      ingredientsH3.textContent = "Ingredients";
+      recipeDiv.appendChild(ingredientsH3);
 
-            const ingredientsUl = document.createElement('ul');
-            recipeDiv.appendChild(ingredientsUl);
+      const ingredientsUl = document.createElement("ul");
+      recipeDiv.appendChild(ingredientsUl);
 
-            recipedata.ingredients.forEach(element => {
-                const li = document.createElement('li');
-                li.textContent = element;
-                ingredientsUl.appendChild(li);
-            });
-        });
+      recipedata.ingredients.forEach((element) => {
+        const li = document.createElement("li");
+        li.textContent = element;
+        ingredientsUl.appendChild(li);
+      });
+    });
 });
 
 // new recipe
@@ -63,83 +63,87 @@ var newRecipeInstructions = [];
 /////////// Fix it so that it pushes to list on button press?? ////////////////
 
 // save ingredients
-document.getElementById('add-ingredient').addEventListener('click', () => {
-    console.log("ingredients saved");
-    let textareaContent = document.getElementById('ingredients-text').value;
-    newRecipeIngredients = textareaContent.split('\n').filter(function(item) {
-        return item.trim() !== '';
-    });
+document.getElementById("add-ingredient").addEventListener("click", () => {
+  console.log("ingredients saved");
+  let textareaContent = document.getElementById("ingredients-text").value;
+  newRecipeIngredients = textareaContent.split("\n").filter(function (item) {
+    return item.trim() !== "";
+  });
 });
 
 // save instructions
-document.getElementById('add-instruction').addEventListener('click', () => {
-    console.log("instructions saved");
-    let textareaContent = document.getElementById('instructions-text').value;
-    newRecipeInstructions = textareaContent.split('\n').filter(function(item) {
-        return item.trim() !== '';
-    });
+document.getElementById("add-instruction").addEventListener("click", () => {
+  console.log("instructions saved");
+  let textareaContent = document.getElementById("instructions-text").value;
+  newRecipeInstructions = textareaContent.split("\n").filter(function (item) {
+    return item.trim() !== "";
+  });
 });
 
 // finally send all in json form
-document.getElementById('recipe-form').addEventListener('submit', function(event) {
+document
+  .getElementById("recipe-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    const newRecipeName = document.getElementById('name-text').value.trim();
+    const newRecipeName = document.getElementById("name-text").value.trim();
 
     let recipe = {
-        name: newRecipeName,
-        ingredients: newRecipeIngredients,
-        instructions: newRecipeInstructions
+      name: newRecipeName,
+      ingredients: newRecipeIngredients,
+      instructions: newRecipeInstructions,
     };
 
-    fetch('/recipe/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(recipe)
+    fetch("/recipe/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recipe),
     })
-    .then(response => {
+      .then((response) => {
         if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error("Network response was not ok " + response.statusText);
         }
         return response.json();
-    })
-    .then(recipeResponse => {
-        console.log('Recipe submitted:', recipeResponse);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-});
-
-
+      })
+      .then((recipeResponse) => {
+        console.log("Recipe submitted:", recipeResponse);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 
 // Image handling
-document.getElementById('image-upload-form').addEventListener('submit', function(event) {
+document
+  .getElementById("image-upload-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    const imagesInput = document.getElementById('image-input');
+    const imagesInput = document.getElementById("image-input");
     const formData = new FormData();
-    
-    for(let i=0; i<imagesInput.isDefaultNamespace.length; i++){
-        formData.append('images', imagesInput.files[i]);
+
+    for (let i = 0; i < imagesInput.isDefaultNamespace.length; i++) {
+      formData.append("images", imagesInput.files[i]);
     }
-    fetch('/images', {
-        method: 'POST',
-        body: formData,
+    fetch("/images", {
+      method: "POST",
+      body: formData,
     })
-    .then(response => {
+      .then((response) => {
         if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.statusText);
+          throw new Error(
+            "Network response was not ok: " + response.statusText
+          );
         }
         return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
-        imagesInput.value = '';
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        imagesInput.value = "";
         // Handle success
-    })
-    .catch(error => {
-        console.error('Error:', error);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
         // Handle errors
-    });
-});
+      });
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
 
 //////////////// API ////////////////
 
-router.get('/recipe/:food', (req, res) => {
+router.get("/recipe/:food", (req, res) => {
   const foodname = req.params.food;
-  const recipedata={ name:foodname, instructions:[], ingredients:[] };
-  res.json( recipedata );
-
+  const recipedata = { name: foodname, instructions: [], ingredients: [] };
+  res.json(recipedata);
 });
 
 // log new recipes
 
 let recipes = [];
 
-router.post('/recipe/', (req,res) =>{
+router.post("/recipe/", (req, res) => {
   let recipe = req.body;
   recipes.push(recipe);
-  console.log('Received recipe:', recipe);
+  console.log("Received recipe:", recipe);
   res.json(recipe);
-})
+});
 
-
-router.post('/images/', (req,res) =>{
+router.post("/images/", (req, res) => {
   let image = req.body;
-  console.log('Received image:', image);
+  console.log("Received image:", image);
   res.json("Hi");
-})
+});
 
 //////////////// API ////////////////
 
